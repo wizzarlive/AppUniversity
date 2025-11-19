0d775d862dbff20cb780c8f01bae32e5de67c976
Wizzar <juniorminano080@gmail.com>
2025-11-19 09:45:27 -0500

    se termino el controlador y modelo de students

--- app/Models/Student.php
+++ app/Models/Student.php
@@ -2,9 +2,43 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Student extends Model
 {
-    //
+    use HasFactory;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'dni',
+        'name',
+        'phone',
+        'ciclo',
+        'email',
+        'status',
+    ];
+    
+    public function tuitions()
+    {
+        return $this->hasMany(Tuition::class, 'FK_STUDENT');
+    }
+
+
+    public function enrollments()
+    {
+        return $this->hasMany(Enrollment::class, 'FK_STUDENT');
+    }
+
+
+    public function courses()
+    {
+        return $this->belongsToMany(Course::class, 'enrollments', 'FK_STUDENT', 'FK_COURSE')
+                    ->withPivot('grade', 'status', 'registration_date')
+                    ->using(Enrollment::class);
+    }
 }
--- database/migrations/2025_11_19_144041_create_students_table.php
+++ database/migrations/2025_11_19_144041_create_students_table.php
@@ -4,15 +4,20 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      */
     public function up(): void
     {
         Schema::create('students', function (Blueprint $table) {
-            $table->id();
+            $table->id(); // ID (Primary Key)
+            $table->string('dni', 15)->unique(); // DNI
+            $table->string('name', 100); // NAME
+            $table->string('phone', 15)->nullable(); // PHONE
+            $table->unsignedSmallInteger('ciclo')->nullable(); // CICLO (usamos smallint para números pequeños)
+            $table->string('email', 100)->unique(); // EMAIL (Único)
+            $table->string('status', 20)->default('activo'); // STATUS
             $table->timestamps();
         });
     }
