00466d1eb8756c463a45d25c5bccab9edee3af86
Wizzar <juniorminano080@gmail.com>
2025-11-19 09:47:07 -0500

    se termino el controlador y modelo de cursos

--- app/Models/Course.php
+++ app/Models/Course.php
@@ -2,9 +2,35 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Course extends Model
 {
-    //
+    use HasFactory;
+
+    protected $fillable = [
+        'name',
+        'fk_teacher',
+    ];
+
+
+    public function teacher()
+    {
+        return $this->belongsTo(Teacher::class, 'fk_teacher');
+    }
+
+
+    public function enrollments()
+    {
+        return $this->hasMany(Enrollment::class, 'FK_COURSE');
+    }
+
+
+    public function students()
+    {
+        return $this->belongsToMany(Student::class, 'enrollments', 'FK_COURSE', 'FK_STUDENT')
+                    ->withPivot('grade', 'status', 'registration_date')
+                    ->using(Enrollment::class);
+    }
 }
