fde0ba4203f0ce02c6f3c22e9ba873dd8094a90d
Wizzar <juniorminano080@gmail.com>
2025-11-19 09:57:33 -0500

    se termino el controlador y modelo de enrollements

--- app/Models/Enrollment.php
+++ app/Models/Enrollment.php
@@ -2,9 +2,30 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Enrollment extends Model
 {
-    //
+    use HasFactory;
+
+    protected $fillable = [
+        'grade',
+        'status',
+        'registration_date',
+        'fk_student',
+        'fk_course',
+    ];
+
+
+    public function student()
+    {
+        return $this->belongsTo(Student::class, 'fk_student');
+    }
+
+
+    public function course()
+    {
+        return $this->belongsTo(Course::class, 'fk_course');
+    }
 }
--- database/migrations/2025_11_19_144130_create_enrollments_table.php
+++ database/migrations/2025_11_19_144130_create_enrollments_table.php
@@ -4,8 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      */
@@ -13,7 +12,17 @@ return new class extends Migration
     {
         Schema::create('enrollments', function (Blueprint $table) {
             $table->id();
+            $table->decimal('grade', 4, 2)->nullable();
+            $table->string('status', 20)->default('pendiente');
+            $table->date('registration_date');
+
+            $table->unsignedBigInteger('fk_student');
+            $table->unsignedBigInteger('fk_course');
             $table->timestamps();
+
+            $table->foreign('fk_student')->references('id')->on('students')->onDelete('cascade');
+            $table->foreign('fk_course')->references('id')->on('courses')->onDelete('cascade');
+
         });
     }
 
