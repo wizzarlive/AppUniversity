a676c57dd4d02f4e5218157eeb3de36b1e05d65d
Wizzar <juniorminano080@gmail.com>
2025-11-19 09:50:38 -0500

    se termino el controlador y modelo de teacher

--- database/migrations/2025_11_19_144111_create_teachers_table.php
+++ database/migrations/2025_11_19_144111_create_teachers_table.php
@@ -4,16 +4,20 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      */
     public function up(): void
     {
         Schema::create('teachers', function (Blueprint $table) {
-            $table->id();
-            $table->timestamps();
+            $table->id(); // ID (Primary Key)
+            $table->string('dni', 15)->unique(); // DNI (Único)
+            $table->string('name', 100); // NAME
+            $table->string('phone', 15)->nullable(); // PHONE (opcional)
+            $table->string('email', 100)->unique(); // EMAIL (Único)
+            $table->string('status', 20)->default('activo'); // STATUS
+            $table->timestamps(); // created_at y updated_at
         });
     }
 
